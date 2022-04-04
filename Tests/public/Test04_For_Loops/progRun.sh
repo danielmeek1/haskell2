d30ea0cfa94060ce735b89624ce13141854e51ca
Ruaraidh-Nicolson <98850567+Ruaraidh-Nicolson@users.noreply.github.com>
2022-04-04 18:46:25 +0100

    Prepared Stacscheck Tests
    Quickcheck doesn't work but Some stacscheck tests work. Warning cabal needs to be installed in the labs for this to work

---
+++ Tests/public/Test04_For_Loops/progRun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./dist-newstyle\build\x86_64-windows\ghc-8.10.7\haskell2-0.1.0.0\x\haskell2\build\haskell2\haskell2 Tests/TestFiles/TestIf
--- app/QuickCheck.hs
+++ app/QuickCheck.hs
@@ -5,10 +5,12 @@ import Test.QuickCheck.All
 import Expr
 import REPL
 import BinaryTree
+import Data.Maybe
 
 
-    
-                       
+-- Check digitsToInt 
+--prop_removeMaybe :: Maybe a -> Bool
+--prop_removeMaybe a = isNothing(removeMaybe a) || not isNothing(removeMaybe a)
 
 
-
+--runTests = $quickCheckAll
