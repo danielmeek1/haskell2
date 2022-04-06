ebc7734ca31ea56d619fdc080952b45115faa6fa
Ruaraidh-Nicolson <98850567+Ruaraidh-Nicolson@users.noreply.github.com>
2022-04-06 15:49:09 +0100

    Added some quickcheck tests that do not work

--- app/QuickCheck.hs
+++ app/QuickCheck.hs
@@ -7,10 +7,23 @@ import REPL
 import BinaryTree
 import Data.Maybe
 
+instance Arbitrary Expr where
+    arbitrary = oneof [return Value Add Value,
+                       return ]
 
--- Check digitsToInt 
+instance Arbitrary Value where
+    arbitrary = oneof [return IntVal,
+                       return FloatVal]
+
+
 --prop_removeMaybe :: Maybe a -> Bool
 --prop_removeMaybe a = isNothing(removeMaybe a) || not isNothing(removeMaybe a)
 
+--prop_Repl_addIntegers :: String -> Bool 
+--prop_Repl_addIntegers str = True
 
---runTests = $quickCheckAll
+prop_Eval :: Expr -> Bool
+prop_Eval e = True
+
+return []
+runTests = $quickCheckAll
